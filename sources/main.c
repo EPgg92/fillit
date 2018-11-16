a5efed110421c892a91aa3f2aa3593ea4dbc7924
Enzo POGGIO <poggio.enzo@gmail.com>
2018-11-16 18:10:41 +0100

    pseudo_code

---
+++ sources/main.c
@@ -0,0 +1,12 @@
+int main(int argc, char **argv)
+{
+	argc++;
+	argv++;
+
+	// parse entry with ft_parser
+	// check formation error.
+	// return struct t_tetrimino list[26];
+	// solve
+
+	return 0;
+}
