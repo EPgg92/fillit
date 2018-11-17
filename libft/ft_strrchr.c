3bc97a04453335becb7e46b1f8ce1db9e963543b
Enzo POGGIO <poggio.enzo@gmail.com>
2018-11-17 09:45:27 +0100

    get_just_func_needed

---
+++ libft/ft_strrchr.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   ft_strrchr.c                                     .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: epoggio <epoggio@student.42.fr>            +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2018/10/28 21:36:11 by epoggio      #+#   ##    ##    #+#       */
+/*   Updated: 2018/10/29 10:55:28 by epoggio     ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	size_t	len;
+
+	len = ft_strlen(s);
+	s += len;
+	if ((unsigned char)c == 0)
+		return ((char *)s);
+	while (len--)
+		if (*(--s) == (unsigned char)c)
+			return ((char *)s);
+	return (NULL);
+}
