38d6c500c8cd5e89519cb978e6ec79b1f753fb66
Enzo POGGIO <poggio.enzo@gmail.com>
2018-11-24 14:00:26 +0100

    add_init_order

--- sources/ft_init.c
+++ sources/ft_init.c
@@ -6,7 +6,7 @@
 /*   By: epoggio <epoggio@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/11/20 14:21:17 by epoggio      #+#   ##    ##    #+#       */
-/*   Updated: 2018/11/20 14:22:15 by epoggio     ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/11/20 15:56:50 by epoggio     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -35,4 +35,15 @@ void	ft_init_output(t_output output)
 	while (++i < LIMIT_SIDE && !(j = -1))
 		while (++j < LIMIT_SIDE)
 			output[i][j] = 0;
+}
+
+void ft_init_order(t_order order, int nb_tetri)
+{
+	int i;
+
+	i = -1;
+	while (++i < NB_TETRI && i <  nb_tetri)
+		order[i] = i;
+	while (++i < NB_TETRI)
+		order[i] = -1;
 }
