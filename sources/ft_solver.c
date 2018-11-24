5ba17de60ebe7688d7e64785f17c4ff68ef6fb91
Enzo POGGIO <poggio.enzo@gmail.com>
2018-11-24 13:59:18 +0100

    first_test_bt

--- sources/ft_solver.c
+++ sources/ft_solver.c
@@ -6,19 +6,34 @@
 /*   By: epoggio <epoggio@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/11/20 14:30:06 by epoggio      #+#   ##    ##    #+#       */
-/*   Updated: 2018/11/20 14:30:22 by epoggio     ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/11/24 13:55:37 by epoggio     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "../includes/fillit.h"
 
-void	ft_solver(t_output output, t_map map, int nb_tetri)
+static int	ft_backtraking(int side, t_map map, t_order order, t_output output)
+{
+
+	//ft_backtraking(side + 1, map, order, output);
+	side++;
+	map[0] = map[0];
+	order[0] = order[0];
+	output[0][0] = 'a';
+
+	return 0;
+}
+
+
+void		ft_solver(t_output output, t_map map, int nb_tetri)
 {
 	int map_size;
+	t_order order;
 
+	ft_init_order(order, nb_tetri);
 	ft_init_output(output);
-	map[0] = map[0];
 	map_size = ft_sqrt(4 * nb_tetri);
+	ft_backtraking(map_size, map, order, output);
 	printf("map_size -> %d\n", map_size);
 }
