875aa834d10672b33664424643670d6923185920
Enzo POGGIO <poggio.enzo@gmail.com>
2018-11-25 17:40:22 +0100

    init_map

--- sources/ft_init.c
+++ sources/ft_init.c
@@ -6,7 +6,7 @@
 /*   By: epoggio <epoggio@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/11/20 14:21:17 by epoggio      #+#   ##    ##    #+#       */
-/*   Updated: 2018/11/24 18:43:41 by epoggio     ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/11/25 17:34:40 by epoggio     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -46,4 +46,13 @@ void ft_init_order(t_order order, int nb_tetri)
 		order[i] = i;
 	while (++i < NB_TETRI)
 		order[i] = -1;
+}
+
+void ft_init_map(t_map map)
+{
+	int i;
+
+	i = -1;
+	while (++i < 16)
+		map[i] = 0;
 }
