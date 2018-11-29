8d676fc18fa8fa11da990e0f8bfd72483420ed05
Valentin Salome <vasalome@z1r5p2.le-101.fr>
2018-11-29 21:53:11 +0100

    norm+new_call

--- sources/ft_main.c
+++ sources/ft_main.c
@@ -15,14 +15,18 @@
 
 int	main(int argc, char **argv)
 {
-	t_tetri		tetri;
-	int			nb_tetri;
+	t_tetri			tetri;
+	int					nb_tetri;
+	t_lst_coord lc;
 
 	if (argc != 2)
 		return (ft_usage());
 	ft_init_tetri(tetri);
 	nb_tetri = ft_parser(tetri, argv[1]);
-	ft_printtetri(tetri, nb_tetri);
+	ft_transform(tetri, nb_tetri, lc);
+	//ft_printtetri(tetri, nb_tetri);
+	//ft_printcoord(lc, nb_tetri);
+	//ft_solver(nb_tetri, lc);
 
 	return (0);
 }
