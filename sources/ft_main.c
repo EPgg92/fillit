ced8a55200a488c0c348dde7056c9f73231512cb
Enzo POGGIO <poggio.enzo@gmail.com>
2018-11-30 17:07:27 +0100

    new container

--- sources/ft_main.c
+++ sources/ft_main.c
@@ -6,7 +6,7 @@
 /*   By: epoggio <epoggio@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/11/16 22:58:00 by epoggio      #+#   ##    ##    #+#       */
-/*   Updated: 2018/11/29 23:43:26 by epoggio     ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/11/30 16:12:21 by epoggio     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -16,17 +16,16 @@
 int	main(int argc, char **argv)
 {
 	t_tetri			tetri;
-	int					nb_tetri;
-	t_lst_coord lc;
+	t_container		c;
 
 	if (argc != 2)
 		return (ft_usage());
 	ft_init_tetri(tetri);
-	nb_tetri = ft_parser(tetri, argv[1]);
-	ft_transform(tetri, nb_tetri, lc);
+	c.nb_tetri = ft_parser(tetri, argv[1]);
+	ft_transform(tetri, c.nb_tetri, c.lc);
 	//ft_printtetri(tetri, nb_tetri);
-	//ft_printcoord(lc, nb_tetri);
-	ft_solver(nb_tetri, lc);
+	//ft_printcoord(c.lc, c.nb_tetri);
+	ft_solver(c);
 
 	return (0);
 }
