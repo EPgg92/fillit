4e174d5a0920950c1116dd7e87526931ee52679a
Enzo POGGIO <poggio.enzo@gmail.com>
2018-11-30 17:06:04 +0100

    new signature and new typedef.

--- includes/fillit.h
+++ includes/fillit.h
@@ -6,7 +6,7 @@
 /*   By: epoggio <epoggio@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/11/20 14:17:57 by epoggio      #+#   ##    ##    #+#       */
-/*   Updated: 2018/11/29 23:47:21 by epoggio     ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/11/30 17:05:31 by epoggio     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -40,9 +40,16 @@ typedef struct    		s_coord
 {
 	t_points        	minos[4];
 	char            	letter;
-	//t_output			out;
 }                 		t_coord;
 typedef t_coord   		t_lst_coord[NB_TETRI];
+
+typedef struct 			s_container
+{
+	int					nb_tetri;
+	int 				side;
+	t_lst_coord			lc;
+	t_output			out;
+}						t_container;
 
 void					ft_error();
 int						ft_usage();
@@ -50,18 +57,13 @@ int						ft_parser(t_tetri tetri, char *fn);
 void					ft_init_tetri(t_tetri tetri);
 void					ft_writechar(char c);
 void					ft_init_output(t_output output);
-void	        ft_re_init_output(t_output init_output, int side);
-int           ft_transform(t_tetri tetri, int nb_tetri, t_lst_coord lc);
-int           ft_solver(int nb_tetri, t_lst_coord lc);
-
-int ft_wipe_piece(t_output out, t_lst_coord lc, int l, int side);
-int ft_change_place_x(t_output out, t_lst_coord lc, int side, int i, int j, int l);
-int ft_change_place_y(t_output out, t_lst_coord lc, int side, int i, int j, int l);
-int ft_place_piece(t_output out, t_lst_coord lc, int side, int i, int j , int l, int nb_tetri);
+void	        		ft_re_init_output(t_output init_output, int side);
+int           			ft_transform(t_tetri tetri, int nb_tetri, t_lst_coord lc);
+int           			ft_solver(t_container c);
+void					ft_printer(t_output out, int side);
 
 // to remove
-void			ft_printcoord(t_lst_coord lc, int nb_tetri);
-void			ft_printtetri(t_tetri tetri, int nb_tetri);
-void			print_out(t_output out);
-
+void					ft_printcoord(t_lst_coord lc, int nb_tetri);
+void					ft_printtetri(t_tetri tetri, int nb_tetri);
+void					print_out(t_output out);
 #endif
