b9e9ea5ce955a73ea5e572b1a2cecfada4725d4d
Enzo POGGIO <poggio.enzo@gmail.com>
2018-11-30 18:33:57 +0100

    norm

--- sources/ft_solver.c
+++ sources/ft_solver.c
@@ -6,7 +6,7 @@
 /*   By: epoggio <epoggio@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/11/20 14:30:06 by epoggio      #+#   ##    ##    #+#       */
-/*   Updated: 2018/11/30 17:54:12 by epoggio     ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/11/30 18:20:40 by epoggio     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -36,7 +36,6 @@ static int	ft_wipe_piece(t_output out, t_lst_coord lc, int l, int side)
 
 static int	ft_place_piece(t_container c, int i, int j, int l)
 {
-	//print_out(c.out);
 	if (c.out[i + c.lc[l].minos[0].x][j + c.lc[l].minos[0].y] == '.'
 		&& c.out[i + c.lc[l].minos[1].x][j + c.lc[l].minos[1].y] == '.'
 		&& c.out[i + c.lc[l].minos[2].x][j + c.lc[l].minos[2].y] == '.'
@@ -61,10 +60,10 @@ static int	ft_place_piece(t_container c, int i, int j, int l)
 	return (0);
 }
 
-static int ft_place_first_piece(t_container c)
+static int	ft_place_first_piece(t_container c)
 {
-	int i;
-	int j;
+	int		i;
+	int		j;
 
 	i = -1;
 	while (++i < c.side && (j = -1) < 0)
@@ -72,7 +71,6 @@ static int ft_place_first_piece(t_container c)
 			ft_place_piece(c, i, j, 0);
 	return (0);
 }
-
 
 static int	ft_backtracking(t_container c)
 {
