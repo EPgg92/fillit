f450fc29f27e03f5f56d03066e224d76023d985b
Enzo POGGIO <poggio.enzo@gmail.com>
2018-11-30 17:18:28 +0100

    clean_spaces

--- libft/srcs/ft_countsetbits.c
+++ libft/srcs/ft_countsetbits.c
@@ -6,20 +6,20 @@
 /*   By: epoggio <epoggio@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/11/17 07:55:37 by epoggio      #+#   ##    ##    #+#       */
-/*   Updated: 2018/11/17 07:55:40 by epoggio     ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/11/30 17:14:11 by epoggio     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
-int ft_countsetbits(unsigned short n)
+int	ft_countsetbits(unsigned short n)
 {
-  int count;
+	int count;
 
-  count = 0;
-  while (n)
-  {
-    count += n & 1;
-    n >>= 1;
-  }
-  return (count);
+	count = 0;
+	while (n)
+	{
+		count += n & 1;
+		n >>= 1;
+	}
+	return (count);
 }
