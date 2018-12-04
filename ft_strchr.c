cea9c036957012eba897db3ad9c629109c41c7b1
Valentin Salome <vasalome@z1r12p4.le-101.fr>
2018-12-04 15:47:42 +0100

    add_libft_files

---
+++ ft_strchr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   ft_strchr.c                                      .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: epoggio <epoggio@student.42.fr>            +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2018/10/28 21:34:40 by epoggio      #+#   ##    ##    #+#       */
+/*   Updated: 2018/10/29 17:35:28 by epoggio     ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include "fillit.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	size_t i;
+	size_t len;
+
+	i = 0;
+	len = ft_strlen(s) + 1;
+	while (i < len)
+	{
+		if (((unsigned char *)s)[i] == (unsigned char)c)
+			return (&((char *)s)[i]);
+		i++;
+	}
+	return (NULL);
+}
---
+++ ft_strsep.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   ft_strsep.c                                      .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: epoggio <epoggio@student.le-101.fr>        +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2018/10/17 02:36:21 by epoggio      #+#   ##    ##    #+#       */
+/*   Updated: 2018/11/14 22:14:53 by epoggio     ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include "fillit.h"
+
+char	*ft_strsep(char **stringp, const char *delim)
+{
+	char *begin;
+	char *end;
+
+	begin = *stringp;
+	if (begin == NULL)
+		return (NULL);
+	end = begin + ft_strcspn(begin, delim);
+	if (*end)
+	{
+		*end++ = '\0';
+		*stringp = end;
+	}
+	else
+		*stringp = NULL;
+	return (begin);
+}
